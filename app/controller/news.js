d761f52740aa59e8336d84b6108886128864bfbe
ShawnGoethe <569326840@qq.com>
2018-12-11 15:31:31 +0800

    add cnodeNews to schedule

--- app/controller/news.js
+++ app/controller/news.js
@@ -1,29 +1,11 @@
 'use strict';
 
 const Controller = require('egg').Controller;
-var cheerio = require('cheerio');
-var request = require('request');
 
 class NewsController extends Controller {
-    async index() {
+    async newsGet() {
         const news = await this.ctx.model.News.find({});
-        request.get('https://cnodejs.org/',function(err,res,sres){
-            if (err) {
-                return next(err);
-            }
-            var $ = cheerio.load(sres.text);
-            var items = [];
-            const tmp = $('#topic_list .topic_title');
-            tmp.each(function (idx, element) {
-                var $element = $(element);
-                items.push({
-                    title: $element.attr('title'),
-                    href: $element.attr('href')
-                });
-            });
-
-            res.send(items);
-        })
+        this.ctx.body = news;
     }
 }
 module.exports = NewsController;
---
+++ app/schedule/updateNews.js
@@ -0,0 +1,45 @@
+const Subscription = require('egg').Subscription;
+var cheerio = require('cheerio');
+var request = require('request');
+
+class UpdateCache extends Subscription {
+  // 通过 schedule 属性来设置定时任务的执行间隔等配置
+  static get schedule() {
+    return {
+      interval: '10000', // 1 分钟间隔
+      type: 'all', // 指定所有的 worker 都需要执行
+    };
+  }
+
+  async subscribe() {
+    const ctx = this.ctx;
+    request.get('https://cnodejs.org/',function(err,res,sres){
+        if (err) {
+            return next(err);
+        }
+        var $ = cheerio.load(sres);
+        var items = [];
+        const tmp = $('#topic_list .topic_title');
+        tmp.each(function (idx, element) {
+            var $element = $(element);
+            items.push({
+                title: $element.attr('title'),
+                href: $element.attr('href')
+            });
+        });
+        const NewsModel = ctx.model.News;
+        NewsModel.insertMany(items,function(err){
+            if(err){
+                ctx.body = err;
+            }else{
+                ctx.body = {
+                    resultCode: 200
+                  }
+            }
+        })
+
+    })
+  }
+}
+
+module.exports = UpdateCache;
