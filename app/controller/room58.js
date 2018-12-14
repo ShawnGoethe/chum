4aa5b3317915a01704afe177fb0c3a5e29b6a10f
JudgDay <851861663@qq.com>
2018-12-14 22:31:59 +0800

    room58

--- app/controller/room58.js
+++ app/controller/room58.js
@@ -4,7 +4,7 @@ const Controller = require('egg').Controller;
 
 class GjController extends Controller {
     async room58() {
-        const gj = await this.ctx.model.Gj.find({});
+        const gj = await this.ctx.model.Room58.find({});
         this.ctx.body = gj;
     }
 }
