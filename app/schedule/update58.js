8aa7459b600d0cb0a6a862d7fb2e63343f10ba20
ShawnGoethe <569326840@qq.com>
2019-01-14 17:42:52 +0800

    change interval inifinity

--- app/schedule/update58.js
+++ app/schedule/update58.js
@@ -6,7 +6,7 @@ class UpdateCache extends Subscription {
     // 通过 schedule 属性来设置定时任务的执行间隔等配置
     static get schedule() {
         return {
-            interval: '1000',
+            interval: '1000000000000',
             type: 'all', // 指定所有的 worker 都需要执行
         };
     }
